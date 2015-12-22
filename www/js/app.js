f3d92e475d6b0d7789abe0cdda992cfffb5f2799
HonoreN <h.nintunze@urbilog.fr>
2015-12-22 15:16:25 +0100

    ex morceaux de phrases

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,11 @@ angular.module('talkApp', ['ionic','talkApp.directives'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    TTS.startup((result)=>{
+      console.log("start ok "+result);
+    }, (result)=>{
+      console.log("start ko "+result);
+    });
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -33,15 +38,15 @@ angular.module('talkApp', ['ionic','talkApp.directives'])
         templateUrl: 'templates/talk.html'
       }
     }
-  });
-  /*.state('app.param', {
+  })
+  .state('app.param', {
       url: '/param',
       views: {
         'talktome': {
           templateUrl: 'templates/param.html'
         }
       }
-    })*/
+    });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/talk');//'/app/playlists');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,47 +1,101 @@
 angular.module('talkApp.directives',[])
+  .controller('optCtrl',['$scope','$ionicPopup', ($scope,$ionicPopup) => {
+    $scope.whichLang = () => {
+      var l;
+      var alertPopup;
+      $scope.time1 = 0;
+      $scope.time2 = 0;
+      $scope.time3 = 0;
+      TTS.getLanguage( (res) => {
+        console.log("whichLang",res);
+        l = res;
+        alertPopup = $ionicPopup.alert({
+          title: 'Langue actuelle',
+          template: 'La langue est : '+l
+        });
 
-  .directive('talkWithMe',['$ionicPlatform', ($ionicPlatform) => {
+        alertPopup.then(function(res) {
+          console.log('Thank you for not eating my delicious ice cream cone');
+        });
+      }, (reason) => {
+        console.log("klnfz",reason);
+      });
+    };
+    $scope.speakSentence = () => {
+      // morceau 1
+      TTS.speak($scope.morceau1,()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
+      if ($scope.time1 > 0)
+        TTS.silence($scope.time1*1000,()=>{console.log("silence ok");},(result)=>{console.log("silence ko "+result);});
+      // morceau 2
+      TTS.speak($scope.morceau2,()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
+      if ($scope.time2 > 0)
+        TTS.silence($scope.time2*1000,()=>{console.log("silence ok");},(result)=>{console.log("silence ko "+result);});
+      // morceau 3
+      TTS.speak($scope.morceau3,()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
+      if ($scope.time3 > 0)
+        TTS.silence($scope.time3*1000,()=>{console.log("silence ok");},(result)=>{console.log("silence ko "+result);});
+    };
+  }])
+  .directive('talkWithMe',['$timeout', ($timeout) => {
     return {
       link: (scope, element, attrs) => {
-        scope.langs = [];
+        scope.langs = [
+          {
+            lang:'en-US'
+          },
+          {
+            lang:"fr-FR"
+          }
+      ];
         scope.tts = "";
+        scope.curLang = {lang:'fr-FR'};
         function win(result) {
           console.log("Win = " + result);
-        }
+        };
+
         function fail(result) {
           console.log("Error = " + result);
-        }
+        };
+
         scope.addLang = (lang) => {
           scope.langs.push({lang:lang});
-        }
+        };
+
 
         scope.changeLang = () => {
           console.log("changing",scope.curLang.lang);
           TTS.setLanguage(scope.curLang.lang,(result)=>{console.log("change ok "+result);}, (result)=>{console.log("change ko "+result);});
-        }
+        };
         // on click on button
         scope.speak = () => {
           console.log("Entered text to speak : "+scope.tts);
           TTS.speak((scope.tts ==""?"Enter a text to speak":scope.tts),()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
-        }
-        $ionicPlatform.ready(() => {
-          console.log('gzegmihxmilqnsmlxnjukegzhflc', TTS);
+        };
+
+        $timeout(() => {
+          //console.log('gzegmihxmilqnsmlxnjukegzhflc', TTS);
           // at start
-          TTS.startup((result)=>{
+          /*TTS.startup((result)=>{
             console.log("start ok "+result);
           }, (result)=>{
             console.log("start ko "+result);
-          });
+          });*/
+
           //scope.curLang = 'fr-FR';
-          TTS.isLanguageAvailable("en", function() {
-            console.log('ajout en ok');
-            scope.addLang("en");
-          }, (result)=>{console.log("ajout en ko "+result);});
-          TTS.isLanguageAvailable("fr", function() {
-            console.log('ajout fr ok');
-            scope.addLang("fr");
-          }, (result)=>{console.log("ajout fr ko "+result);});
-          if (!scope.curLang) {
+          /*TTS.isLanguageAvailable("en", function(res) {
+            console.log('ajout en ok',res);
+            scope.addLang("en-US");
+          }, (result)=>{
+            console.log("ajout en ko "+result);
+          });
+
+          TTS.isLanguageAvailable("fr", function(res) {
+            console.log('ajout fr ok',res);
+            scope.addLang("fr-FR");
+          }, (result)=>{
+            console.log("ajout fr ko "+result);
+          });*/
+          if (!scope.curLang.lang) {
             TTS.setLanguage(
               "fr",
               (result)=>{
@@ -51,11 +105,17 @@ angular.module('talkApp.directives',[])
                 console.log("set debut ko "+result);
               }
             );
-            scope.curLang = "fr";
           }
-          TTS.speak("The text to speech service is ready",()=>{console.log("speak ok ");},(result)=>{console.log("speak ko "+result);});
+
+          TTS.speak("The text to speech service is ready",(res)=>{
+            console.log("speak ok ",res);
+          },(result)=>{
+              console.log("speak ko ",result);
+            }
+          );
+          scope.$apply();
           console.log("langue: ",scope.curLang,scope.langs);
-        })
+        },2000);
 
 
       }
