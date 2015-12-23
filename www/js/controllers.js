93e8a334239cd4cf7da8288b7a74869847286bcd
nash403 <honore.nintunze@gmail.com>
2015-12-23 11:11:57 +0100

    version 1.0.0 de l'appli test de TTS

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,17 @@
 angular.module('talkApp.directives',[])
   .controller('optCtrl',['$scope','$ionicPopup', ($scope,$ionicPopup) => {
-    $scope.whichLang = () => {
-      var l;
-      var alertPopup;
       $scope.time1 = 0;
       $scope.time2 = 0;
       $scope.time3 = 0;
+      $scope.interruptTxt = "";
+      $scope.pitch = 5;
+      $scope.vitesse = 5;
+      $scope.testSP = "";
+      var longTxt = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    $scope.whichLang = () => {
+      var l;
+      var alertPopup;
       TTS.getLanguage( (res) => {
-        console.log("whichLang",res);
         l = res;
         alertPopup = $ionicPopup.alert({
           title: 'Langue actuelle',
@@ -15,38 +19,77 @@ angular.module('talkApp.directives',[])
         });
 
         alertPopup.then(function(res) {
-          console.log('Thank you for not eating my delicious ice cream cone');
+          console.log('whichLang ok',res);
         });
       }, (reason) => {
-        console.log("klnfz",reason);
+        console.log("whichLang ko",reason);
       });
     };
     $scope.speakSentence = () => {
       // morceau 1
-      TTS.speak($scope.morceau1,()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
+      $scope.play($scope.morceau1);
       if ($scope.time1 > 0)
-        TTS.silence($scope.time1*1000,()=>{console.log("silence ok");},(result)=>{console.log("silence ko "+result);});
+        TTS.silence($scope.time1*1000,()=>{console.log("silence ok");},(result)=>{console.log("silence ko ",result);});
       // morceau 2
-      TTS.speak($scope.morceau2,()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
+      $scope.play($scope.morceau2);
       if ($scope.time2 > 0)
-        TTS.silence($scope.time2*1000,()=>{console.log("silence ok");},(result)=>{console.log("silence ko "+result);});
+        TTS.silence($scope.time2*1000,()=>{console.log("silence ok");},(result)=>{console.log("silence ko ",result);});
       // morceau 3
-      TTS.speak($scope.morceau3,()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
+      $scope.play($scope.morceau3);
       if ($scope.time3 > 0)
-        TTS.silence($scope.time3*1000,()=>{console.log("silence ok");},(result)=>{console.log("silence ko "+result);});
+        TTS.silence($scope.time3*1000,()=>{console.log("silence ok");},(result)=>{console.log("silence ko ",result);});
+    };
+    $scope.play = (txt) => {
+      console.log("play",txt);
+      if (typeof text == 'string'){
+        TTS.speak((txt==''?longTxt:txt),()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
+      }
+      else {
+        if (txt.text==''){
+          TTS.speak({
+            text:longTxt,
+            speed:$scope.vitesse*0.2,
+            pitch:$scope.pitch*0.2
+          },()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
+        }
+        else {
+          TTS.speak(txt,()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
+        }
+      }
+    };
+    $scope.inter = () => {
+      console.log("interruption",$scope.interruptTxt);
+      TTS.interrupt($scope.interruptTxt,()=>{console.log("interrupt ok");},(result)=>{console.log("interrupt ko ",result);});
+    };
+    $scope.stop = () => {
+      console.log("stop");
+      TTS.stop(()=>{console.log("interrupt ok");},(result)=>{console.log("interrupt ko ",result);});
+    };
+    $scope.isAvailable = () => {
+      console.log("isAvailable",$scope.lg);
+      TTS.isLanguageAvailable($scope.lg,(res)=>{
+        alertPopup = $ionicPopup.alert({
+          title: 'Langue disponible ?',
+          template: 'La langue \''+$scope.lg+'\' est '+res?"disponible.":"non disponible."
+        });
+
+        alertPopup.then(function(res) {
+          console.log('isLanguageAvailable ok',res);
+        });
+      },(result)=>{console.log("isLanguageAvailable ko ",result);});
     };
   }])
   .directive('talkWithMe',['$timeout', ($timeout) => {
     return {
       link: (scope, element, attrs) => {
         scope.langs = [
-          {
-            lang:'en-US'
-          },
-          {
-            lang:"fr-FR"
-          }
-      ];
+            {
+              lang:'en-US'
+            },
+            {
+              lang:"fr-FR"
+            }
+          ];
         scope.tts = "";
         scope.curLang = {lang:'fr-FR'};
         function win(result) {
@@ -64,37 +107,15 @@ angular.module('talkApp.directives',[])
 
         scope.changeLang = () => {
           console.log("changing",scope.curLang.lang);
-          TTS.setLanguage(scope.curLang.lang,(result)=>{console.log("change ok "+result);}, (result)=>{console.log("change ko "+result);});
+          TTS.setLanguage(scope.curLang.lang,(result)=>{console.log("change ok ",result);}, (result)=>{console.log("change ko ",result);});
         };
         // on click on button
         scope.speak = () => {
-          console.log("Entered text to speak : "+scope.tts);
-          TTS.speak((scope.tts ==""?"Enter a text to speak":scope.tts),()=>{console.log("speak ok");},(result)=>{console.log("speak ko "+result);});
+          console.log("texte entré : "+scope.tts);
+          TTS.speak((scope.tts ==""?"Entrez un texte à vocaliser":scope.tts),()=>{console.log("speak ok");},(result)=>{console.log("speak ko ",result);});
         };
 
         $timeout(() => {
-          //console.log('gzegmihxmilqnsmlxnjukegzhflc', TTS);
-          // at start
-          /*TTS.startup((result)=>{
-            console.log("start ok "+result);
-          }, (result)=>{
-            console.log("start ko "+result);
-          });*/
-
-          //scope.curLang = 'fr-FR';
-          /*TTS.isLanguageAvailable("en", function(res) {
-            console.log('ajout en ok',res);
-            scope.addLang("en-US");
-          }, (result)=>{
-            console.log("ajout en ko "+result);
-          });
-
-          TTS.isLanguageAvailable("fr", function(res) {
-            console.log('ajout fr ok',res);
-            scope.addLang("fr-FR");
-          }, (result)=>{
-            console.log("ajout fr ko "+result);
-          });*/
           if (!scope.curLang.lang) {
             TTS.setLanguage(
               "fr",
@@ -107,7 +128,7 @@ angular.module('talkApp.directives',[])
             );
           }
 
-          TTS.speak("The text to speech service is ready",(res)=>{
+          TTS.speak("Le service TTS est pret",(res)=>{
             console.log("speak ok ",res);
           },(result)=>{
               console.log("speak ko ",result);
